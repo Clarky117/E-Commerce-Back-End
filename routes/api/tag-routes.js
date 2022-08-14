31399a05e8434dfeed7b5c117110cdf663034146
Clarky <clarky117@outlook.com>
2022-08-14 12:31:22 +0800

    all tag routes working

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,23 +6,115 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: {
+      model: Product,
+      attributes: ['product_name', 'price', 'stock', 'category_id'],
+      as: 'products'
+    }
+  }).then((tags) => {
+    res.status(200).json(tags)
+  }).catch((error) => {
+    console.log(error);
+    res.status(500).json({
+      error: "We have encountered an error. Please try again later."
+    })
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findByPk(req.params.id, {
+    include: [
+      {
+        model: Product,
+        attributes: ['product_name', 'price', 'stock', 'category_id'],
+      }
+    ]
+  })
+    // .then(category => res.json(tag))
+    .then((tag) => {
+      if (!tag) {
+        res.status(404)
+          .json({
+            message: 'There is no tag at this id'
+          });
+        return;
+      }
+      res.status(200).json(tag)
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        error: "We have encountered an error. Please try again later."
+      })
+    })
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  }).then(tag => {
+    res.status(200).json(tag)
+  })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        error: "We have encountered an error. Please try again later."
+      })
+    })
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update({
+    tag_name: req.body.tag_name
+  }, {
+    where: {
+      id: req.params.id
+    }
+  }).then((tag => {
+    if (!tag) {
+      res.status(404)
+        .json({
+          message: 'There is no tag at this id'
+        });
+      return;
+    }
+    res.status(200).json(tag)
+  }
+  ))
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        error: "We have encountered an error. Please try again later."
+      })
+    })
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then((tag) => {
+    if (!tag) {
+      res.status(404)
+        .json({
+          message: 'There is no tag at this id'
+        });
+      return;
+    }
+    res.status(200).json(tag)
+  }).catch((error) => {
+    console.log(error);
+    res.status(500).json({
+      error: "We have encountered an error. Please try again later."
+    })
+  })
 });
 
 module.exports = router;
